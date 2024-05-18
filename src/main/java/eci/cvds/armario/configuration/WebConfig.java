1a6aa069ed68170cd5ca1f070a29708954704b01
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2024-05-18 08:38:58 -0500

    auth with headers working

--- src/main/java/eci/cvds/armario/configuration/WebConfig.java
+++ src/main/java/eci/cvds/armario/configuration/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(basicAuthInterceptor)
-                .addPathPatterns("/user/*");
+                .addPathPatterns("/user/**");
     }
 }
