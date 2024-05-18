5606a1580a92cd46256427c09f9a56d31092c56e
Daniel <dani.07rojas94@gmail.com>
2024-05-17 22:12:23 -0500

    uso authToken ignorando el interceptor

--- src/main/java/eci/cvds/armario/configuration/WebConfig.java
+++ src/main/java/eci/cvds/armario/configuration/WebConfig.java
@@ -2,9 +2,11 @@ package eci.cvds.armario.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final AuthInterceptor basicAuthInterceptor;
@@ -16,7 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(basicAuthInterceptor)
-                .addPathPatterns("/user/*");
+        registry.addInterceptor(basicAuthInterceptor).addPathPatterns("/user/*");
     }
 }
