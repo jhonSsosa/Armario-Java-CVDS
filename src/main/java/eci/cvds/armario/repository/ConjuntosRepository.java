9f9119374c8a9593785b2541c006e4c48c335982
jhonSsosa <jhon.sosa-m@mail.escuelaing.edu.co>
2024-10-22 18:32:38 -0500

    Cambios 12

--- src/main/java/eci/cvds/armario/repository/ConjuntosRepository.java
+++ src/main/java/eci/cvds/armario/repository/ConjuntosRepository.java
@@ -2,7 +2,6 @@ package eci.cvds.armario.repository;
 import eci.cvds.armario.model.Conjuntos;
 import eci.cvds.armario.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 public interface ConjuntosRepository extends JpaRepository<Conjuntos, UUID>{
