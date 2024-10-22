e250b3d51c5606c8378f3ee897d106807885d947
jhonSsosa <jhon.sosa-m@mail.escuelaing.edu.co>
2024-10-22 18:39:38 -0500

    Cambios 16

--- src/main/java/eci/cvds/armario/repository/SessionRepository.java
+++ src/main/java/eci/cvds/armario/repository/SessionRepository.java
@@ -1,7 +1,6 @@
 package eci.cvds.armario.repository;
 
 import eci.cvds.armario.model.Session;
-import eci.cvds.armario.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
