048d02f863cee67fbf826bb81fac7a9bea4942a4
jhonSsosa <jhon.sosa-m@mail.escuelaing.edu.co>
2024-10-22 18:02:46 -0500

    Cambios 2

--- src/main/java/eci/cvds/armario/configuration/AuthInterceptor.java
+++ src/main/java/eci/cvds/armario/configuration/AuthInterceptor.java
@@ -62,10 +62,6 @@ public class AuthInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-    }
-
-    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
     }
 }
